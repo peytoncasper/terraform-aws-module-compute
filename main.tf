683ff8b771366ec2f27bf0a17b4c215527d0c06c
Peyton Casper <peytoncas@gmail.com>
2021-07-15 18:59:20 -0400

    Added security group ids to network interface instead of vm

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 resource "aws_network_interface" "subnet" {
   subnet_id   = var.subnet_id
   private_ips = ["10.0.10.100"]
-
+  security_groups = var.security_group_ids
+  
   tags = {
     Name = "primary_network_interface"
   }
@@ -10,8 +11,6 @@ resource "aws_network_interface" "subnet" {
 resource "aws_instance" "web" {
   ami           = "ami-03368e982f317ae48"
   instance_type = "t3.medium"
-
-  vpc_security_group_ids = var.security_group_ids
 
   network_interface {
     network_interface_id = aws_network_interface.subnet.id
