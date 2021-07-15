ccf3c4d2f44b5b4410361fe0606dd2447e98c099
Peyton Casper <peytoncas@gmail.com>
2021-07-15 18:10:58 -0400

    Adding network interface and subnet id variables

--- main.tf
+++ main.tf
@@ -1,8 +1,23 @@
+resource "aws_network_interface" "subnet" {
+  subnet_id   = var.subnet_id
+  private_ips = ["10.0.10.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
 resource "aws_instance" "web" {
   ami           = "ami-03368e982f317ae48"
   instance_type = var.instance_type
 
   vpc_security_group_ids = var.security_group_ids
+
+  network_interface {
+    network_interface_id = aws_network_interface.subnet.id
+    device_index         = 0
+  }
+
 
   associate_public_ip_address = false
 }
--- variable.tf
+++ variable.tf
@@ -1,3 +1,7 @@
 variable "security_group_ids" {
     type=list(string)
 }
+
+variable "subnet_id" {
+    type=string
+}
